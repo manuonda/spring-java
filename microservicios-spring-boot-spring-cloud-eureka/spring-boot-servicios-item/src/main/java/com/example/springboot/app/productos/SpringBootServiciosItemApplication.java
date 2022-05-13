5eb5541d6d657ff14568be6f78b9bd0f748df5a8
manuonda <manuonda@gmail.com>
2022-05-13 17:36:07 -0300

    initi project items

---
+++ microservicios-spring-boot-spring-cloud-eureka/spring-boot-servicios-item/src/main/java/com/example/springboot/app/productos/SpringBootServiciosItemApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServiciosItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServiciosItemApplication.class, args);
+	}
+
+}
---
+++ microservicios-spring-boot-spring-cloud-eureka/spring-boot-servicios-item/src/main/java/com/example/springboot/app/productos/models/Producto.java
@@ -0,0 +1,5 @@
+package com.example.springboot.app.productos.models;
+
+public class Producto {
+
+}
