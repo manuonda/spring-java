16a7356bd77addd0bcf5e19da1e76d9ce3645be8
dgarcia <manuonda@gmail.com>
2022-08-21 21:18:43 -0300

    modificte files

--- study-projects/data-projection-patron/src/main/java/com/data/projection/repository/PersonRepository.java
+++ study-projects/data-projection-patron/src/main/java/com/data/projection/repository/PersonRepository.java
@@ -2,21 +2,19 @@ package com.data.projection.repository;
 
 import java.util.Optional;
 
+import javax.persistence.Tuple;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.data.projection.dto.PersonLocationDTO2;
 import com.data.projection.entity.Person;
+import com.data.projection.projections.PersonFullLocation;
 import com.data.projection.projections.PersonLocation;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
-	
-	// Close Projection
-	// select firstName, lastName from person inner join address a on
-	// a.id = p.address_id
-	// where p.id := ?1
-	//PersonLocation findByUserName(String firstName);
 	
 	//Close projection
 	@Query(value = "SELECT " +
@@ -28,4 +26,44 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 	" INNER JOIN ADDRESS A ON A.ID = P.ADDRESS_ID "+
 	" WHERE P.ID = :id", nativeQuery=true)
     PersonLocation getPersonLocation(@Param("id") Long personId);
+	
+	//Open Projection Sample
+	@Query(value = "SELECT " +
+	"P.FIRST_NAME as firstName," +
+    "P.LAST_NAME as lastName," + 
+	"P.PHONE_NUMBER as phoneNumber, "+
+	"A.STREET as street " + 
+	" FROM PERSON P "+
+	" INNER JOIN ADDRESS A ON A.ID = P.ADDRESS_ID "+
+	" WHERE P.ID = :id", nativeQuery=true)
+    PersonFullLocation getPersonFullLocation(@Param("id") Long personId);
+	
+	// Using class based
+	@Query(value = "SELECT " +
+			"P.FIRST_NAME as firstName," +
+		    "P.LAST_NAME as lastName," + 
+			"P.PHONE_NUMBER as phoneNumber, "+
+			"A.STREET as street " + 
+			" FROM PERSON P "+
+			" INNER JOIN ADDRESS A ON A.ID = P.ADDRESS_ID "+
+			" WHERE P.ID = :id", nativeQuery=true) 
+	Tuple getPersonLocationDTO(@Param("id") Long personId);
+	
+	
+	// using named query 
+	@Query(name="getPersonLocationDTONamingQuery", nativeQuery = true)
+	public PersonLocationDTO2 getPersonLocationDTO2(@Param("id") Long personId);
+	
+	// dynamically
+	@Query(value = "SELECT " +
+			"P.FIRST_NAME as firstName," +
+		    "P.LAST_NAME as lastName," + 
+			"P.PHONE_NUMBER as phoneNumber, "+
+			"A.STREET as street " + 
+			" FROM PERSON P "+
+			" INNER JOIN ADDRESS A ON A.ID = P.ADDRESS_ID "+
+			" WHERE P.ID = :id", nativeQuery=true) 
+	<T> T getPersonLocationDynamically(@Param("id") Long personId, Class<T> type);
+	
+	
 }
--- study-projects/data-projection-patron/src/main/java/com/data/projection/service/IPerson.java
+++ study-projects/data-projection-patron/src/main/java/com/data/projection/service/IPerson.java
@@ -2,9 +2,20 @@ package com.data.projection.service;
 
 import java.util.Optional;
 
+import com.data.projection.dto.PersonDTO;
+import com.data.projection.dto.PersonLocationDTO2;
+import com.data.projection.projections.PersonFullLocation;
 import com.data.projection.projections.PersonLocation;
 
 public interface IPerson {
 
 	Optional<PersonLocation> getCloseProjection(Long idPerson);
+	
+	Optional<PersonFullLocation> getPersonFullLocation(Long idPerson);
+	
+	PersonDTO getPersonLocationDTO(Long idPerson);
+	
+	PersonLocationDTO2 getPersonLocationDTO2(Long idPerson);
+	<T> T getPersonLocationDynamically(Long id, Class<PersonLocation> class1);
+	
 }
