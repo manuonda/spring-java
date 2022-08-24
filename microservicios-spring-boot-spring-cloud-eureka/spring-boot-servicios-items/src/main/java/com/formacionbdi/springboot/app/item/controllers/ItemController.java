df1b644b22a7ac3f2a9cdd629298e640579cd14c
dgarcia <manuonda@gmail.com>
2022-08-23 21:54:43 -0300

    files configuration

--- microservicios-spring-boot-spring-cloud-eureka/spring-boot-servicios-items/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ microservicios-spring-boot-spring-cloud-eureka/spring-boot-servicios-items/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,5 +1,6 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -7,7 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,9 +27,10 @@ import com.formacionbdi.springboot.app.item.services.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import io.vavr.collection.Map;
 
 
-
+@RefreshScope
 @RestController
 @RequestMapping("/api/items")
 public class ItemController {
@@ -32,11 +39,19 @@ public class ItemController {
 	
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
+	
+	@Autowired 
+	Environment env;
+	
+    //@Value("${configuracion.texto}")
+	//private String texto;
 
 	@Autowired
 	// con el qualifier me permite indicar que servicio trabajar
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+
  
 	
 	
@@ -109,5 +124,22 @@ public class ItemController {
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return CompletableFuture.supplyAsync( ()-> item);
+	}
+	
+	@GetMapping("/get-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		//logger.info(texto);
+	    HashMap<String, String> json = new HashMap<>();
+		//json.put("texto",texto);
+		json.put("puerto", puerto);
+		//json.put("configuracionAutor",configuracionAutorNombre);
+		
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+		   	json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+		   	json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<HashMap<String,String>>(json, HttpStatus.OK);
+		
 	}
 }
