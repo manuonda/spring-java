e6767a176b6950808f83250dfdc120d87f7f5e41
dgarcia <manuonda@gmail.com>
2023-02-19 01:12:09 -0300

    modificate files

---
+++ junit5/junit5-testing/TestingRestController/src/test/java/com/testingrest/TestingRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
