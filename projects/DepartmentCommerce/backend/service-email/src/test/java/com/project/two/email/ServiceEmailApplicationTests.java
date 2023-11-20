a419e9db11acd89a08a913373b57b7c3d0ba1700
dgarcia <manuonda@gmail.com>
2023-11-20 10:43:18 -0300

    Develop: add project service-email

---
+++ projects/DepartmentCommerce/backend/service-email/src/test/java/com/project/two/email/ServiceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.two.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
