2fdcba39d4e2635e988d79925bd0755379584898
dgarcia <manuonda@gmail.com>
2023-12-11 11:55:43 -0300

    TODO: usuairos

--- projects/DepartmentCommerce/backend/servicio-usuarios/src/main/java/com/projec/two/usuarios/ServicioUsuariosApplication.java
+++ projects/DepartmentCommerce/backend/servicio-usuarios/src/main/java/com/projec/two/usuarios/ServicioUsuariosApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServicioUsuariosApplication {
 
+	// TODO: realizar una registration de usuairo y enviar
+	// un email para activar el usuario y tambien
+	// subir la imagen a S3
+	// la comunicacion se realiza con el enviom de email kafka s3
+	// la comunicacion de subidad de archivo usa openfeign
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioUsuariosApplication.class, args);
 	}
