93e8249ea944fcfe898733ea8471ce0623048216
dgarcia <manuonda@gmail.com>
2023-12-13 18:40:23 -0300

    aaa

--- projects/DepartmentCommerce/backend/servicio-usuarios/src/main/java/com/projec/two/usuarios/business/service/UsuarioService.java
+++ projects/DepartmentCommerce/backend/servicio-usuarios/src/main/java/com/projec/two/usuarios/business/service/UsuarioService.java
@@ -1,7 +1,13 @@
 package com.projec.two.usuarios.business.service;
 
+import com.projec.two.usuarios.domain.dto.UsuarioDTO;
+
 public interface UsuarioService {
 
-    UsuarioDTO save(UsuarioDTO usuarioDTO);;
+    UsuarioDTO save(UsuarioDTO usuarioDTO);
+
+    UsuarioDTO update(UsuarioDTO usuarioDTO);
+
+    
 
 }
