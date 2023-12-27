82690f94cee25187c50ee81b15b650c18c8f56b5
David Garcia <david.garcia@grupomost.com>
2023-12-26 21:25:40 -0300

    Develop spring java

--- projects/course-docker-kubernetes/microservice-usuarios/src/main/java/com/docker/kubernetes/usuarios/presentation/controller/UsuarioController.java
+++ projects/course-docker-kubernetes/microservice-usuarios/src/main/java/com/docker/kubernetes/usuarios/presentation/controller/UsuarioController.java
@@ -4,7 +4,9 @@ package com.docker.kubernetes.usuarios.presentation.controller;
 import com.docker.kubernetes.usuarios.business.impl.UsuarioService;
 import com.docker.kubernetes.usuarios.domain.dto.UsuarioDTO;
 import com.docker.kubernetes.usuarios.domain.entity.Usuario;
+import feign.Response;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.List;
 
 @RequestMapping("/api/v1/usuarios")
+@RestController
 public class UsuarioController {
 
 
@@ -23,23 +26,26 @@ public class UsuarioController {
 
     @GetMapping("/")
     public ResponseEntity<List<UsuarioDTO>> findAll(){
-        return this.usuarioService.findAll();
+        List<UsuarioDTO> list = this.usuarioService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
     @PostMapping("/save")
     public ResponseEntity<UsuarioDTO> saveUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO){
-
-
+        UsuarioDTO usuario = this.usuarioService.save(usuarioDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(usuario);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<UsuarioDTO> update(@RequestBody @Valid UsuarioDTO usuarioDTO,
                                              @PathVariable("id") Long id){
-
+     UsuarioDTO usuario = this.usuarioService.update(usuarioDTO, id);
+     return ResponseEntity.status(HttpStatus.OK).body(usuario);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id){
-
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+      this.usuarioService.delete(id);
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
