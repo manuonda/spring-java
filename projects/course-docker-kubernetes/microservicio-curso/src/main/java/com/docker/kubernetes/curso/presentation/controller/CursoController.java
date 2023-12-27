84b29b7e04639ee771696c5c5e4c450868199fc8
David Garcia <david.garcia@grupomost.com>
2023-12-27 20:56:17 -0300

    Modificacion de Curso Controller

--- projects/course-docker-kubernetes/microservicio-curso/src/main/java/com/docker/kubernetes/curso/presentation/controller/CursoController.java
+++ projects/course-docker-kubernetes/microservicio-curso/src/main/java/com/docker/kubernetes/curso/presentation/controller/CursoController.java
@@ -3,6 +3,8 @@ package com.docker.kubernetes.curso.presentation.controller;
 
 import com.docker.kubernetes.curso.business.service.CursoService;
 import com.docker.kubernetes.curso.domain.dto.CursoDTO;
+import com.docker.kubernetes.curso.domain.entity.Curso;
+import feign.Response;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +34,23 @@ public class CursoController {
     public ResponseEntity<?> save(@RequestBody @Valid CursoDTO dto){
         CursoDTO save = this.cursoService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(save);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@RequestBody @Valid CursoDTO dto, @PathVariable("id") Long id){
+        CursoDTO update = this.cursoService.update(dto, id);
+        return ResponseEntity.status(HttpStatus.OK).body(update);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+        this.cursoService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CursoDTO> findById(@PathVariable("id") Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(this.cursoService.findById(id));
     }
 
 }
