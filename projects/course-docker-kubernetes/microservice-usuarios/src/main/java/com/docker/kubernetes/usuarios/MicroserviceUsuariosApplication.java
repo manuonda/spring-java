6d754f20ac6151b72d7c9708724ccee2a636b7dd
david garcia <david.garcia@grupomost.com>
2024-01-26 15:25:43 -0300

    Develop : add files de kuberentes

--- projects/course-docker-kubernetes/microservice-usuarios/src/main/java/com/docker/kubernetes/usuarios/MicroserviceUsuariosApplication.java
+++ projects/course-docker-kubernetes/microservice-usuarios/src/main/java/com/docker/kubernetes/usuarios/MicroserviceUsuariosApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.docker.kubernetes.usuarios.domain.entity")
-@EnableJpaRepositories(basePackages = "com.docker.kubernetes.usuarios.repository")
 public class MicroserviceUsuariosApplication {
 
 	public static void main(String[] args) {
