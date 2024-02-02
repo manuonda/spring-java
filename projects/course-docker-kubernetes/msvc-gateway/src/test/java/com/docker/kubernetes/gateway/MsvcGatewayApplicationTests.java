13275946bf15df652b6beb0b260228351a84f2b8
david garcia <david.garcia@grupomost.com>
2024-02-02 17:12:57 -0300

    Project cloud gw

---
+++ projects/course-docker-kubernetes/msvc-gateway/src/test/java/com/docker/kubernetes/gateway/MsvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.kubernetes.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
